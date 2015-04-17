f93553c639541fe6e37e00ed083f02d07938a754
James Dunkerley <james.dunkerley@gmail.com>
2015-04-17 06:50:48 +0100

    Initial Clean MVC Site

---
+++ Site/App_Start/BundleConfig.cs
@@ -0,0 +1,18 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace Site
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/js").Include(
+                      "~/Scripts/jdunkerley.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/site.css"));
+        }
+    }
+}
